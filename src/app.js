ecc6a4382a8f2fa8895c54ca81acc71b1af1bde3
Eriko <eriko.918k@gmail.com>
2021-12-24 13:08:09 +0900

    Showing current temperature

---
+++ src/app.js
@@ -0,0 +1,20 @@
+function displayTemperature(response) {
+    console.log(response.data);
+    let cityElement = document.querySelector("#city");
+    let temperatureElement = document.querySelector("#temperature");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+
+}
+
+let apiKey = "9656d85472f993f571de9d36d411cd1e";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
