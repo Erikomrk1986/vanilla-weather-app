c1ced2046da1bc9b9a2e647f0b383d9b99fab198
Eriko <eriko.918k@gmail.com>
2021-12-31 19:52:30 +0900

    Revert "Revert "Updated JSS""
    This reverts commit ee4ee85ec5f25549915f7f7c03e8a225824dfb61.

--- src/app.js
+++ src/app.js
@@ -27,31 +27,52 @@ function formatDate(timestamp) {
   return `${currentLocalTime}`;
 }
 
-// Display forecast 
-function displayForecast(response) {
-  console.log(response.data.daily);
-  let forecastElement = document.querySelector("#forecast");
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = [
+    "Sun",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat",
+  ];
 
+  return days[day];
+}
+
+
+// Display daily forecast 
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+  
   let forecastHTML = `<div class = "row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
-     forecastHTML = forecastHTML +
-     `
+  forecast.forEach(function (forecastDay, index) {
+     if (index < 6) {
+     forecastHTML = 
+       forecastHTML +
+       `
      <div class = "col-2">
-        <div class ="weather-forecast-date">${day}</div>
+        <div class ="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        
         <img 
-          src="https://openweathermap.org/img/wn/04n@2x.png" 
+          src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" 
           alt""
           width="42"
         />
         <div class = "weather-forecast-temperatures">
-          <span class = "weather-forecast-temperature-max"> 18° </
+          <span class = "weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}° </
           span>
-          <span class = "weather-forecast-temperature-min"> 12° </
+          <span class = "weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)}° </
           span>              
         </div>              
      </div>
   `; 
+    }
   })
  
   forecastHTML = forecastHTML + `</div>`;
@@ -128,14 +149,15 @@ function displayCelsiusTemperature(event) {
 }
 
 let celsiusTemperature = null;
-
+// FORM 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
+// FAHRENHEIT
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
+// CELSIUS 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
-
-search("New York");
+// SEARCH
+search("Bangkok");
