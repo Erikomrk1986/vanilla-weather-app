265f576b259928c21e2154497fb954de6d13995d
Eriko <eriko.918k@gmail.com>
2021-12-31 16:01:47 +0900

    Removed HTML then injected to JS
    function displayForecast and etc

--- src/app.js
+++ src/app.js
@@ -27,6 +27,35 @@ function formatDate(timestamp) {
   return `${currentLocalTime}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class = "row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+     forecastHTML = forecastHTML +
+     `
+     <div class = "col-2">
+        <div class ="weather-forecast-date">${day}</div>
+        <img 
+          src="https://openweathermap.org/img/wn/04n@2x.png" 
+          alt""
+          width="42"
+        />
+        <div class = "weather-forecast-temperatures">
+          <span class = "weather-forecast-temperature-max"> 18° </
+          span>
+          <span class = "weather-forecast-temperature-min"> 12° </
+          span>              
+        </div>              
+     </div>
+  `; 
+  })
+ 
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;       
+}
+
 function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   let temperatureElement = document.querySelector("#temperature");
@@ -98,3 +127,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+displayForecast();
